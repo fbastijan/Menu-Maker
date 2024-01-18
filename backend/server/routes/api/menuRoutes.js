b5315df6886372e9a44eff84c6d3197deb6d3ff5
Filip Bastijanić <59928451+filipbasti@users.noreply.github.com>
2024-01-18 12:42:02 +0100

    promjenio arhitekturu baze

--- backend/server/routes/api/menuRoutes.js
+++ backend/server/routes/api/menuRoutes.js
@@ -21,7 +21,31 @@ let menu_item = {
 }
 */
 
-//inicijalizacija
+/*
+{
+    "title": "My Restaurant Menu",
+    "categories":["fish", "pizza", "pasta"]
+    
+ 
+
+}
+
+
+
+      {
+        "name": "Spaghetti Bolognese",
+        "description": "Classic Italian pasta with meat sauce",
+        "price": 12.99,
+        "categoriesIndex": 2  // Reference to predefined item type
+      },
+      {
+        "name": "Custom Pasta Dish",
+        "description": "User-defined pasta dish",
+        "price": 15.99,
+        "categoriesIndex": 2  // Reference to user-defined item type
+      },
+*/
+
 router.get("/menu/:userId", async (req, res) => {
   try {
     const db = await connectToMongoDB();
@@ -46,6 +70,43 @@ router.get("/menu/:userId", async (req, res) => {
     });
   }
 });
+
+router.get("/menu/:userId/kategorija/:index", async (req, res) => {
+  try {
+    const db = await connectToMongoDB();
+    const collection = db.collection("menu");
+    try {
+      console.log(req.params.userId);
+      console.log(req.params.index);
+
+      let result = await collection
+        .find(
+          {
+            userId: req.params.userId,
+            "current.items.categoriesIndex": req.params.index,
+          },
+          { projection: { "current.items.$": 1 } }
+        )
+        .toArray();
+      res.status(200).json({
+        success: "true",
+        msg: "pronšao si menu",
+        menu: result,
+      });
+      return;
+    } catch {
+      return res.status(404).json({
+        msg: "User does not have a menu or there is an error with finding it",
+      });
+    }
+  } catch {
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
+});
+
+//inicijaliziraj menu
 router.post("/menu", verifyToken, async (req, res) => {
   let newMenu = {
     userId: req.userId,
@@ -73,6 +134,27 @@ router.post("/menu", verifyToken, async (req, res) => {
       msg: "Internal server error",
     });
   }
+});
+router.post("/menu/:id/item", verifyToken, async (req, res) => {
+  const db = await connectToMongoDB();
+  const collection = db.collection("menuItems");
+  let pom = req.body.items.forEach((element) => {
+    element.menuId = req.params.id;
+  });
+  collection
+    .insertMany(pom)
+    .then((pom) => {
+      return res.status(201).json({
+        success: "true",
+        msg: "Dodao si item na menu" + pom,
+      });
+    })
+    .catch((err) => {
+      console.error("Greška pri dodavanju menu-a:", err);
+      return res.status(500).json({
+        msg: "Internal server error",
+      });
+    });
 });
 //dodavanje na menu
 router.patch("/menu");
